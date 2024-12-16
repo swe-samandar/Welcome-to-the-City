3210ae5a5fff8b8e4d45fcd24921decc0eeaccea
Samandar Nemataliyev <samandarnemataliyev@gmail.com>
2024-12-16 10:23:03 +0500

    initial commit

---
+++ testing.py
@@ -0,0 +1,7 @@
+from greeting import say_hello
+
+print(say_hello(['John', 'Smith'], 'Phoenix', 'Arizona'))
+print(say_hello(['Franklin','Delano','Roosevelt'], 'Chicago', 'Illinois'))
+print(say_hello(['Wallace','Russel','Osbourne'],'Albany','New York'))
+print(say_hello(['Lupin','the','Third'],'Los Angeles','California'))
+print(say_hello(['Marlo','Stanfield'],'Baltimore','Maryland'))
